c728afef7aaab2230f3add10ea39d198e7bc29b5
Jesus Fernandez <jesus.fndz@gmail.com>
2016-05-29 11:32:18 -0400

    Initial port of Nocturnal into Laravel Spark
    * Creates the basic layout for nocturnal using Laravel Spark
    * Uses less (nocturnal.less) for css modifications
    * Attempts to mantain spark files without modifications
    
    Signed-off-by: Jesus Fernandez <jesus.fndz@gmail.com>

---
+++ resources/views/vendor/spark/nav/brand.blade.php
@@ -0,0 +1,3 @@
+<a class="navbar-brand" href="/home">
+    <img src="/img/nocturnal-innovations-logo.png" style="height: 32px;">
+</a>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,47 @@
+@extends('spark::layouts.app')
+
+@section('title', 'Nocturnal Innovations')
+
+@section('content')
+    <div id="tag-line-container" class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h1 id="tag-line"><a href="/">So, how did you <span class="honey">sleep</span>?</a></h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-9">
+                <h1>About Us</h1>
+                <p>We are a group of web enthusiasts looking to help you grow your business online.</p>
+                <p>We love and support Open Web Standards , they help us turn your next project into a total success. Since 2003, Nocturnal Innovations has been helping small business succeed using tools available to us thanks to the Open Web Standards. This is an important part of our culture and we are happy to be part of this community.</p>
+                <h1>Our Services</h1>
+                <h4>Web Application Development</h4>
+                <p>Just as with a traditional desktop application, web applications have varying levels of risk. From a personal homepage to your recruitment tool, we specialize in building and maintaining web applications built using the latest technologies and security measures available.</p>
+            </div>
+            <div class="col-md-3">
+                <h1>Contact Us</h1>
+                <p>Nocturnal Innovations.</p>
+
+                <p>P.O. Box 1165<br/>
+                Goldenrod, FL 32733-1165</p>
+
+                <p>Tel: (407) 278-5476<br/>
+                Mail: support@nocturnalinc.com</p>
+            </div>
+        </div>
+    </div>
+    <div id="footer-container" class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h1>So why are we good for you?</h1>
+                    <blockquote>Your success becomes our success, We are here for you, just tell us what you need and we will recommend the best course of action that will benefit and fit you and your company. By helping you succeed, we succeed.</blockquote>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
